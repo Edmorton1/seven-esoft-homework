b7db57ad9dd61b400f2c2af2401d5e5bdc488640
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-05-31 19:42:49 +0500

    finaly

--- grep-lite.js
+++ grep-lite.js
@@ -1,5 +1,8 @@
-// node grep-lite.js --file=text.txt --search=hello
+//  node grep-lite.js --file="text.txt, file.txt"  --search=man --ignore-case=true
+
+
 const fs = require('fs')
+// const path = require('path')
 
 const rawArgs = process.argv
 
@@ -11,30 +14,36 @@ if (checkArgs.length < 2) {
 }
 
 const args = Object.assign({}, ...rawArgs.slice(2).map(e => e.split('--')[1].split('=')).map(e => ({ [e[0]]: e[1] })))
+const [fileNames, search, ignoreCase] = [args.file.split(',').map(e => e.trim()), args.search, args["ignore-case"] === "true"]
+// console.log(fileNames, search, ignoreCase)
 
-try {
+// console.log(args)
+// console.log(fileNames)
+
+const files = fileNames.map(file => {
+  try {
+    return {name: file, data: fs.readFileSync(file, 'utf-8').split('\n')}
+  } catch(err) {
+    if (err.errno === -4058) {
+      console.log(`[ERROR]: File not found`)
+      process.exit(1)
+    }
+  }
+})
+
+// console.log(`${path.resolve(__dirname, 'text.txt')}, ${path.resolve(__dirname, 'file.txt')}`)
+// console.log(files)
+
+const regex = new RegExp(args.search, ignoreCase ? 'gi' : 'g')
+
+files.forEach(file => {
+  // console.log(file.data, file.name)
+  console.log(`\x1b[46m----[FILE]: ${file.name} -----\x1b[0m`)
   
-} catch {
-
-}
-
-console.log(args.ignore_case)
-
-try {
-  fs.openSync(args.file)
-} catch(err) {
-  if (err.errno === -4058) {
-    console.log(`[ERROR]: File not found`)
-    process.exit(1)
-  }
-}
-
-const file = fs.readFileSync(args.file, 'utf-8').split('\n').filter(e => e.trim() !== '')
-// console.log(file)
-
-file.map((e, i) => {
-  // console.log(i, e)
-  if (e.includes(args.search)) {
-    // console.log(`[Line: ${i}]: ${e}`)
-  }
+  file.data.forEach((e, i) => {
+    if (regex.test(e)) {
+      const word = e.replace(regex, match => `\x1b[35m${match}\x1b[0m`)
+      console.log(`[Line: ${i + 1}]: ${word}`)
+    }
+  })
 })
